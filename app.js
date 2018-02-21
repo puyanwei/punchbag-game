e12613b6ff6c5dbd948fe44b6967e595769e4e7f
Puyan Wei <puyanwei@gmail.com>
2018-02-21 11:03:38 +0000

    when health is at zero, change picture to burst version

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ new Vue({
   },
   methods: {
     punch: function() {
-      console.log(this.health, this.ended);
       this.health -= 10;
       if (this.health <= 0) {
         this.ended = true;
@@ -15,7 +14,6 @@ new Vue({
     restart: function() {
       this.health = 100;
       this.ended = false;
-      console.log(this.health, this.ended);
     }
   },
   computed: {}
