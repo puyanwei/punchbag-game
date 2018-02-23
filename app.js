748abc21c1a160a15bd51b1e80ad35ebfdf8eefd
Puyan Wei <puyanwei@gmail.com>
2018-02-23 11:09:25 +0000

    added batman segway music when you press restart button

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-new Vue({
-  el: "#app",
-  data: {
-    health: 100,
-    ended: false,
-    // prettier-ignore
-    batman: [
-      "AIEEE!!!!", "AIIEEE!!!!", "ARRGH!!!!", "AWK!!!!", "AWKKKKKK!!!!", "BAM!!!!", "BANG!!!!", "BANG - ETH!!!!", "BIFF!!!!", "BLOOP!!!!", "BLURP!!!!", "BOFF!!!!", "BONK!!!!", "CLANK!!!!", "CLANK - EST!!!!", "CLASH!!!!", "CLUNK!!!!", "CLUNK - ETH!!!!", "CRRAACK!!!!", "CRASH!!!!", "CRRAACK!!!!", "CRUNCH!!!!", "CRUNCH - ETH!!!!", "EEE - YOW!!!!", "FLRBBBBB!!!!", "GLIPP!!!!", "GLURPP!!!!", "KAPOW!!!!", "KAYO!!!!", "KER - SPLOOSH!!!!", "KERPLOP!!!!", "KLONK!!!!", "KLUNK!!!!", "KRUNCH!!!!", "OOOFF!!!!", "OOOOFF!!!!", "OUCH!!!!", "OUCH - ETH!!!!", "OWWW!!!!", "OW - ETH!!!!", "PAM!!!!", "PLOP!!!!", "POW!!!!", "POWIE!!!!", "QUNCKKK!!!!", "RAKKK!!!!", "RIP!!!!", "SLOSH!!!!", "SOCK!!!!", "SPLATS!!!!", "SPLATT!!!!", "SPLOOSH!!!!", "SWAAP!!!!", "SWISH!!!!", "SWOOSH!!!!", "THUNK!!!!", "THWACK!!!!", "THWACKE!!!!", "THWAPE!!!!", "THWAPP!!!!", "UGGH!!!!", "URKKK!!!!", "VRONK!!!!", "WHACK!!!!", "WHACK - ETH!!!!", "WHAM - ETH!!!!", "WHAMM!!!!", "WHAMMM!!!!", "WHAP!!!!", "Z - ZWAP!!!!", "ZAM!!!!", "ZAMM!!!!", "ZAMMM!!!!", "ZAP!!!!", "ZAP - ETH!!!!", "ZGRUPPP!!!!", "ZLONK!!!!", "ZLOPP!!!!", "ZLOTT!!!!", "ZOK!!!!", "ZOWIE!!!!", "ZWAPP!!!!", "ZZWAP!!!!", "ZZZZWAP!!!!", "ZZZZZWAP!!!!"
-    ],
-    chosenWord: ""
-  },
-  methods: {
-    punch: function() {
-      this.health -= 10;
-      if (this.health <= 0) {
-        this.ended = true;
-        this.playKnockout();
+window.onload = function() {
+  new Vue({
+    el: "#app",
+    data: {
+      health: 100,
+      ended: false,
+      // prettier-ignore
+      batman: [
+        "AIEEE!!!!", "AIIEEE!!!!", "ARRGH!!!!", "AWK!!!!", "AWKKKKKK!!!!", "BAM!!!!", "BANG!!!!", "BANG - ETH!!!!", "BIFF!!!!", "BLOOP!!!!", "BLURP!!!!", "BOFF!!!!", "BONK!!!!", "CLANK!!!!", "CLANK - EST!!!!", "CLASH!!!!", "CLUNK!!!!", "CLUNK - ETH!!!!", "CRRAACK!!!!", "CRASH!!!!", "CRRAACK!!!!", "CRUNCH!!!!", "CRUNCH - ETH!!!!", "EEE - YOW!!!!", "FLRBBBBB!!!!", "GLIPP!!!!", "GLURPP!!!!", "KAPOW!!!!", "KAYO!!!!", "KER - SPLOOSH!!!!", "KERPLOP!!!!", "KLONK!!!!", "KLUNK!!!!", "KRUNCH!!!!", "OOOFF!!!!", "OOOOFF!!!!", "OUCH!!!!", "OUCH - ETH!!!!", "OWWW!!!!", "OW - ETH!!!!", "PAM!!!!", "PLOP!!!!", "POW!!!!", "POWIE!!!!", "QUNCKKK!!!!", "RAKKK!!!!", "RIP!!!!", "SLOSH!!!!", "SOCK!!!!", "SPLATS!!!!", "SPLATT!!!!", "SPLOOSH!!!!", "SWAAP!!!!", "SWISH!!!!", "SWOOSH!!!!", "THUNK!!!!", "THWACK!!!!", "THWACKE!!!!", "THWAPE!!!!", "THWAPP!!!!", "UGGH!!!!", "URKKK!!!!", "VRONK!!!!", "WHACK!!!!", "WHACK - ETH!!!!", "WHAM - ETH!!!!", "WHAMM!!!!", "WHAMMM!!!!", "WHAP!!!!", "Z - ZWAP!!!!", "ZAM!!!!", "ZAMM!!!!", "ZAMMM!!!!", "ZAP!!!!", "ZAP - ETH!!!!", "ZGRUPPP!!!!", "ZLONK!!!!", "ZLOPP!!!!", "ZLOTT!!!!", "ZOK!!!!", "ZOWIE!!!!", "ZWAPP!!!!", "ZZWAP!!!!", "ZZZZWAP!!!!", "ZZZZZWAP!!!!"
+      ],
+      chosenWord: ""
+    },
+    methods: {
+      punch: function() {
+        this.health -= 10;
+        if (this.health <= 0) {
+          this.ended = true;
+          this.playKnockout();
+        }
+      },
+      restart: function() {
+        this.health = 100;
+        this.ended = false;
+        this.chosenWord = "";
+      },
+      playPunch: function() {
+        var punch = new Audio("/assets/sounds/punch.mp3");
+        punch.play();
+      },
+      playKnockout: function() {
+        var knockout = new Audio("/assets/sounds/knockout.wav");
+        var fainted = new Audio("/assets/sounds/fainted.mp3");
+        fainted.play();
+        knockout.play();
+      },
+      playSegway: function() {
+        var segway = new Audio("/assets/sounds/segway.mp3");
+        segway.play();
+      },
+      pickWords: function() {
+        this.chosenWord = this.batman[
+          Math.floor(Math.random() * this.batman.length)
+        ];
       }
-    },
-    restart: function() {
-      this.health = 100;
-      this.ended = false;
-      this.chosenWord = "";
-    },
-    playPunch: function() {
-      var punch = new Audio("/assets/sounds/punch.mp3");
-      punch.play();
-    },
-    playKnockout: function() {
-      var knockout = new Audio("/assets/sounds/knockout.wav");
-      var fainted = new Audio("/assets/sounds/fainted.mp3");
-      fainted.play();
-      knockout.play();
-    },
-    pickWords: function() {
-      this.chosenWord = this.batman[
-        Math.floor(Math.random() * this.batman.length)
-      ];
     }
-  }
-});
+  });
+};
